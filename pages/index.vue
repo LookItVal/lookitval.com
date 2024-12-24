8002145481eb2e3e816f2cf36d24c326381ccc7e
LookItVal <86853474+LookItVal@users.noreply.github.com>
2024-12-23 18:27:59 -0600

    Add contact and home pages, update app layout, and include global stylesheet

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div class="navbar">
+        <NuxtLink to="/">
+            <img src="~/assets/QVC.svg" alt="QVC" height="100%" style="color: var(--mauve)">
+        </NuxtLink>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>

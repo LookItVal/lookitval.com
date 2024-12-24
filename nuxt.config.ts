2437eca92e5bed8068ba571dabb2f5e6244138e2
LookItVal <86853474+LookItVal@users.noreply.github.com>
2024-12-24 16:27:48 -0600

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: process.env.NODE_ENV === "production" ? "/lookitval.github.io/" : "/",
     buildAssetsDir: "assets",
   },
   compatibilityDate: '2024-11-01',

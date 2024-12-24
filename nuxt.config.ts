b4b786f0128354ecc5227e8ce020db8b8cb74aea
LookItVal <86853474+LookItVal@users.noreply.github.com>
2024-12-24 16:17:36 -0600

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    cdnURL: 'https://raw.githubusercontent.com/LookItVal/lookitval.github.io/refs/heads/main/docs/'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/stylesheet.css']

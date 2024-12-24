5c392c3c75187089e31001e77d6c1d6fdfb690ee
LookItVal <86853474+LookItVal@users.noreply.github.com>
2024-12-23 22:38:40 -0600

    roughed in other sections

---
+++ components/Home/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="navbar">
+        <Logo />
+        <div height="100%">
+            <NuxtLink to="/portfolio" height="100%">
+                <button>Portfolio</button>
+            </NuxtLink>
+            <NuxtLink to="/contact" height="100%">
+                <button>Contact</button>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeNavbar'
+}
+</script>
+
+<style scoped>
+.logo {
+    display: block;
+    height: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="home">
-    <div class="navbar">
-        <Logo />
-        <div height="100%">
-            <NuxtLink to="/portfolio" height="100%">
-                <button>Portfolio</button>
-            </NuxtLink>
-            <NuxtLink to="/contact" height="100%">
-                <button>Contact</button>
-            </NuxtLink>
-        </div>
-    </div>
+    <HomeNavbar />
     <div class="landing">
         <span class="background-lambda">λ</span>
         <div class="left-text">
@@ -20,6 +10,22 @@
         <div class="right-image">
             <img src="~assets/BoomOp.png" alt="Quinn Valencia Cecil" />
         </div>
+    </div>
+    <div class="about">
+        <div class="left-image">
+            <img src="~assets/SoundGirl.jpg" alt="Quinn Valencia Cecil" />
+        </div>
+        <div class="right-text">
+            <h2>About Me</h2>
+            <p>Hi! I'm Quinn, a full stack developer with a background in audio engineering. I love creating things, whether it's a new song or a new app. I'm passionate about accessibility and making the web a better place for everyone. I'm currently looking for new opportunities to grow and learn.</p>
+            <button>Resume</button>
+        </div>
+    </div>
+    <div class="skills">
+        <h2>Skills</h2>
+    </div>
+    <div class="portfolio">
+        <h2>Portfolio</h2>
     </div>
   </div>
 </template>
@@ -31,22 +37,27 @@ export default {
 </script>
 
 <style scoped>
-    .home {
-        overflow: hidden;
-    }
-
     .landing {
         display: flex;
         justify-content: space-between;
         align-items: center;
         height: 100vh;
         padding: 0 10%;
+        overflow-y: hidden;
+    }
+
+    .landing h1 {
+        color: var(--lavender);
+    }
+
+    .landing h1 span {
+        color: var(--mauve);
     }
 
     .background-lambda {
         translate: 0 -20%;
         font-size: 120vh;
-        color: var(--crust);
+        color: var(--base);
         position: absolute;
         top: 0;
         left: 0;
@@ -70,5 +81,43 @@ export default {
         padding-bottom: 50%;
         object-fit: cover;
         object-position: bottom;
+    }
+
+    .about {
+        background-color: var(--mantle);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 60vh;
+        padding: 5% 10%;
+    }
+
+    .left-image {
+        width: 25%;
+    }
+    .left-image img {
+        width: 100%;
+    }
+    
+    .right-text {
+        width: 75%;
+    }
+
+    .skills {
+        background-color: var(--crust);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 60vh;
+        padding: 5% 10%;
+    }
+
+    .portfolio {
+        background-color: var(--mantle);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 60vh;
+        padding: 5% 10%;
     }
 </style>

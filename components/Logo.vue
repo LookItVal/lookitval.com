9e95c1a29a883b8d9b003dc467f8c93e159e040b
LookItVal <86853474+LookItVal@users.noreply.github.com>
2024-12-30 12:18:09 -0600

    better masking on logo shimmer

--- components/Logo.vue
+++ components/Logo.vue
@@ -5,8 +5,17 @@
                 <mask id="logo-mask" height="100%" width="100%">
                     <image href="~assets/QVC.svg" height="100%" width="100%" />
                 </mask>
+                <mask id="outer-shimmer-mask" height="100%" width="100%">
+                    <image href="~assets/QVCHILIGHTPATH1.svg" height="100%" width="100%" />
+                </mask>
+                <mask id="inner-shimmer-mask" height="100%" width="100%">
+                    <image href="~assets/QVCHILIGHTPATH2.svg" height="100%" width="100%" />
+                </mask>
                 <radialGradient id="radial-gradient" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
                     <stop offset="0%" style="stop-color:var(--red);stop-opacity:1" />
+                    <stop offset="25%" style="stop-color:var(--red);stop-opacity:0.5" />
+                    <stop offset="50%" style="stop-color:var(--red);stop-opacity:0.4" />
+                    <stop offset="75%" style="stop-color:var(--red);stop-opacity:0.1" />
                     <stop offset="100%" style="stop-color:var(--red);stop-opacity:0" />
                 </radialGradient>
                 <radialGradient id="logo-gradient" cx="20%" cy="0%" r="150%" fx="50%" fy="50%">
@@ -16,6 +25,9 @@
             </defs>
             <g mask="url(#logo-mask)">
                 <rect width="100%" height="100%" fill="url(#logo-gradient)" />
+                <circle r="0.1" cx="21" cy="11" fill="var(--red)"></circle>
+            </g>
+            <g mask="url(#outer-shimmer-mask)">
                 <path id="logo-path-1" d="M 9.7,7.7
                                                L 7.4,2 
                                                C 7.2,1.6, 6.6,1.25, 6.25,1.25 
@@ -25,11 +37,6 @@
                                                C 1.2,8.5, 2,8.75, 2.3,8.75
                                                H 9
                                                L 10,10" 
-                stroke="none" fill="none" />
-                <path id="logo-path-2" d="M 9.85,7.8
-                                               L 12.2,2.1
-                                               L 12.2,2
-                                               L 12.5,0" 
                 stroke="none" fill="none" />
                 <path id="logo-path-3" d="M 9.7,7.7
                                                L 9.82,8
@@ -47,13 +54,21 @@
                     </animateMotion>
                 </circle>
                 <circle r="1" fill="url(#radial-gradient)">
-                    <animateMotion id="anim2" dur="0.25s" fill="freeze">
-                        <mpath href="#logo-path-2" />
-                    </animateMotion>
-                </circle>
-                <circle r="1" fill="url(#radial-gradient)">
                     <animateMotion id="anim3" dur="1s" fill="freeze">
                         <mpath href="#logo-path-3" />
+                    </animateMotion>
+                </circle>
+                <circle r="0.1" cx="21" cy="11" fill="var(--red)"></circle>
+            </g>
+            <g mask="url(#inner-shimmer-mask)">
+                <path id="logo-path-2" d="M 9.85,7.8
+                                               L 12.2,2.1
+                                               L 12.2,2
+                                               L 12.5,0" 
+                stroke="none" fill="none" />
+                <circle r="1" fill="url(#radial-gradient)">
+                    <animateMotion id="anim2" dur=".25s" fill="freeze">
+                        <mpath href="#logo-path-2" />
                     </animateMotion>
                 </circle>
                 <circle r="0.1" cx="21" cy="11" fill="var(--red)"></circle>

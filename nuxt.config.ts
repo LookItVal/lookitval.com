d134f707b97ed261301e0abc5c2b4861708f54fe
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-06-09 16:06:02 -0500

    build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ['/', '/portfolio', '/portfolio/academic'],
+      routes: ['/'],
       ignore: []
     }
   }

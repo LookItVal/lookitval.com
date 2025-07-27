9cb21e98ad42a9215003e2e72883718bd230d162
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-07-26 23:58:31 -0500

    moved in theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: [
+    '@/assets/main.css'
+  ],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',

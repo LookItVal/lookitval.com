29bff002f193e768871b3ff42591b57c0800051b
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-09-09 22:50:15 -0500

    better entry animation for feather loading animation

--- app/components/UI/FeatherLoadingBar.vue
+++ app/components/UI/FeatherLoadingBar.vue
@@ -2,20 +2,24 @@
   <div>
     <svg class="w-full h-full" viewBox="0 0 533.7909 148.63465" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
       <path 
-        id="barbsPath"
+        ref="barbsPathUnloaded"
         d="m 529.50146,74.317533 c 0,-5.99344 -2.80863,-11.84121 -8.34881,-17.38139 -19.45463,-19.45463 -69.92014,-32.06447 -98.27017,-37.84931 l -2.2493,-0.45891 -34.49692,26.73712 10.55145,-31.083 -5.88737,-0.90792 C 352.78062,7.5164432 311.70479,4.2906232 275.14171,4.2906232 c -6.70973,-7e-4 -13.42371,0.1089 -19.95596,0.32386 l -1.35341,0.0438 -74.0468,46.6294498 28.78773,-43.2240298 -11.23876,1.7550402 c -6.627,1.0352096 -13.09491,2.2266796 -19.22552,3.5418996 l -1.11441,0.239 -51.92426,40.24286 9.37199,-27.60827 -11.36603,6.36679 c -15.03875,8.42447 -24.887332,30.04125 -29.14765,35.46582 l -77.138986,3.5e-4 c -6.9034828,3.6e-4 -12.4995268,2.79838 -12.5002338,6.25012 7.07e-4,1.7257 1.399365,3.2884 3.661399,4.41942 2.2620348,1.13102 5.3874468,1.83034 8.8388348,1.8307 h 77.105752 c 2.127684,2.71493 5.666047,16.89154 10.606604,21.832097 7e-4,7e-4 7e-4,7e-4 0.001,0.001 4.87055,4.87055 11.11996,9.45685 18.57216,13.63231 l 11.36603,6.36821 -9.3727,-27.610397 51.92427,40.242857 1.11369,0.23829 c 6.11648,1.31381 12.58509,2.50458 19.22624,3.54261 l 11.23875,1.75645 -28.78702,-43.224727 74.04822,46.629447 1.35411,0.0446 c 6.54852,0.21425 13.26249,0.32385 19.95597,0.32385 51.162,0 108.00844,-6.02808 155.96371,-16.53922 l 27.27523,-6.78186 0.15203,-0.0403 c 47.09048,-13.30209 70.96806,-29.002687 70.96736,-46.665507 z"
         :stroke="unloadedStrokeColorValue"
-        :stroke-width="props.strokeWidth"
+        stroke-width="0"
         stroke-linejoin="round"
+        stroke-linecap="round"
         fill="none"
+        stroke-dasharray="0 10000"
       />
       <path
-        id="shaftPath"
+        ref="shaftPathUnloaded"
         d="m 16.789645,68.067193 c -6.9034798,3.6e-4 -12.4995298,2.79838 -12.5002398,6.25012 0.002,4.83442 8.4900358,5.97908 12.5002398,6.25012 171.423775,-1.5636 330.142755,-2.82794 499.720075,-6.23799 -170.15453,-5.83033 -328.69625,-4.77835 -499.720075,-6.26225 z"
         :stroke="unloadedStrokeColorValue"
-        :stroke-width="props.strokeWidth"
+        stroke-width="0"
         stroke-linejoin="round"
+        stroke-linecap="round"
         fill="none"
+        stroke-dasharray="0 10000"
       />
 
       <!-- Animated Paths -->
@@ -23,7 +27,7 @@
         ref="barbsPathLoaded"
         d="m 529.50146,74.317533 c 0,-5.99344 -2.80863,-11.84121 -8.34881,-17.38139 -19.45463,-19.45463 -69.92014,-32.06447 -98.27017,-37.84931 l -2.2493,-0.45891 -34.49692,26.73712 10.55145,-31.083 -5.88737,-0.90792 C 352.78062,7.5164432 311.70479,4.2906232 275.14171,4.2906232 c -6.70973,-7e-4 -13.42371,0.1089 -19.95596,0.32386 l -1.35341,0.0438 -74.0468,46.6294498 28.78773,-43.2240298 -11.23876,1.7550402 c -6.627,1.0352096 -13.09491,2.2266796 -19.22552,3.5418996 l -1.11441,0.239 -51.92426,40.24286 9.37199,-27.60827 -11.36603,6.36679 c -15.03875,8.42447 -24.887332,30.04125 -29.14765,35.46582 l -77.138986,3.5e-4 c -6.9034828,3.6e-4 -12.4995268,2.79838 -12.5002338,6.25012 7.07e-4,1.7257 1.399365,3.2884 3.661399,4.41942 2.2620348,1.13102 5.3874468,1.83034 8.8388348,1.8307 h 77.105752 c 2.127684,2.71493 5.666047,16.89154 10.606604,21.832097 7e-4,7e-4 7e-4,7e-4 0.001,0.001 4.87055,4.87055 11.11996,9.45685 18.57216,13.63231 l 11.36603,6.36821 -9.3727,-27.610397 51.92427,40.242857 1.11369,0.23829 c 6.11648,1.31381 12.58509,2.50458 19.22624,3.54261 l 11.23875,1.75645 -28.78702,-43.224727 74.04822,46.629447 1.35411,0.0446 c 6.54852,0.21425 13.26249,0.32385 19.95597,0.32385 51.162,0 108.00844,-6.02808 155.96371,-16.53922 l 27.27523,-6.78186 0.15203,-0.0403 c 47.09048,-13.30209 70.96806,-29.002687 70.96736,-46.665507 z"
         :stroke="loadedStrokeColorValue"
-        :stroke-width="props.strokeWidth"
+        stroke-width="0"
         stroke-linejoin="round"
         stroke-linecap="round"
         fill="none"
@@ -33,7 +37,7 @@
         ref="shaftPathLoaded"
         d="m 16.789645,68.067193 c -6.9034798,3.6e-4 -12.4995298,2.79838 -12.5002398,6.25012 0.002,4.83442 8.4900358,5.97908 12.5002398,6.25012 171.423775,-1.5636 330.142755,-2.82794 499.720075,-6.23799 -170.15453,-5.83033 -328.69625,-4.77835 -499.720075,-6.26225 z"
         :stroke="loadedStrokeColorValue"
-        :stroke-width="props.strokeWidth"
+        stroke-width="0"
         stroke-linejoin="round"
         stroke-linecap="round"
         fill="none"
@@ -69,10 +73,45 @@ const loadedStrokeColorValue = computed(() => COLORS[props.loadedStrokeColor]);
 
 const barbsPathLoaded = ref<SVGPathElement>();
 const shaftPathLoaded = ref<SVGPathElement>();
+const barbsPathUnloaded = ref<SVGPathElement>();
+const shaftPathUnloaded = ref<SVGPathElement>();
 
-const animationTimeline = ref<GSAPTimeline | null>(null);
+let loadingAnimationTimeline: GSAPTimeline
+let entryAnimationTimeline: GSAPTimeline
 
-onMounted(() => {
+onMounted(async () => {
+  await nextTick();
+  if (barbsPathUnloaded.value && shaftPathUnloaded.value) {
+    const barbsLength = barbsPathUnloaded.value.getTotalLength();
+    const shaftLength = shaftPathUnloaded.value.getTotalLength();
+
+    gsap.set([barbsPathUnloaded.value, shaftPathUnloaded.value], {
+      strokeDasharray: function(index) {
+        return index === 0 ? `0 ${barbsLength}` : `0 ${shaftLength}`;
+      },
+      strokeWidth: 0
+    });
+
+    entryAnimationTimeline = gsap.timeline({ ease: props.easeFunction });
+
+    entryAnimationTimeline.to(barbsPathUnloaded.value, {
+      strokeWidth: props.strokeWidth,
+      duration: 0.5
+    }, 0);
+    entryAnimationTimeline.to(barbsPathUnloaded.value, {
+      strokeDasharray: `${barbsLength} 0`,
+      duration: 1.5
+    }, '>');
+
+    entryAnimationTimeline.to(shaftPathUnloaded.value, {
+      strokeWidth: props.strokeWidth,
+      duration: 0.5
+    }, 0);
+    entryAnimationTimeline.to(shaftPathUnloaded.value, {
+      strokeDasharray: `${shaftLength} 0`,
+      duration: 1.5
+    }, '>');
+  }
   if (barbsPathLoaded.value && shaftPathLoaded.value) {
     const barbsLength = barbsPathLoaded.value.getTotalLength();
     const shaftLength = shaftPathLoaded.value.getTotalLength();
@@ -81,29 +120,36 @@ onMounted(() => {
     gsap.set([barbsPathLoaded.value, shaftPathLoaded.value], {
       strokeDasharray: function(index) {
         return index === 0 ? `0 ${barbsLength}` : `0 ${shaftLength}`;
-      }
+      },
+      strokeWidth: 0
     });
 
     // Create a timeline for the loading animation
-    animationTimeline.value = gsap.timeline({ paused: true });
+    loadingAnimationTimeline = gsap.timeline({ paused: true, ease: props.easeFunction });
 
-    animationTimeline.value.to(barbsPathLoaded.value, {
+    loadingAnimationTimeline.to([barbsPathLoaded.value, shaftPathLoaded.value], {
+      strokeWidth: props.strokeWidth,
+      duration: 0.5
+    }, 0);
+    loadingAnimationTimeline.to(barbsPathLoaded.value, {
       strokeDasharray: `${barbsLength} 0`,
-      duration: 2,
-      ease: props.easeFunction
-    }, 0);
+      duration: 5
+    }, '>');
 
-    animationTimeline.value.to(shaftPathLoaded.value, {
-      strokeDasharray: `${shaftLength} 0`,
-      duration: 2,
-      ease: props.easeFunction
+    loadingAnimationTimeline.to(shaftPathLoaded.value, {
+      strokeWidth: props.strokeWidth,
+      duration: 0.5
     }, 0);
+    loadingAnimationTimeline.to(shaftPathLoaded.value, {
+      strokeDasharray: `${shaftLength} 0`,
+      duration: 5
+    }, '>');
   }
 });
 
 watch(() => props.progress, (newProgress) => {
-  if (animationTimeline.value) {
-    gsap.to(animationTimeline.value, {
+  if (loadingAnimationTimeline) {
+    gsap.to(loadingAnimationTimeline, {
       progress: newProgress,
       duration: 1,
       ease: props.easeFunction
@@ -112,9 +158,13 @@ watch(() => props.progress, (newProgress) => {
 });
 
 onUnmounted(() => {
-  if (animationTimeline.value) {
-    animationTimeline.value.kill();
-    animationTimeline.value = null;
+  if (entryAnimationTimeline.value) {
+    entryAnimationTimeline.value.kill();
+    entryAnimationTimeline.value = null;
+  }
+  if (loadingAnimationTimeline.value) {
+    loadingAnimationTimeline.value.kill();
+    loadingAnimationTimeline.value = null;
   }
 });
 </script>

8cd307c7fefa3a81ff8b29febbe9b30060c8a86c
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-10-11 16:27:27 -0500

    fix: streamline project loading logic and update data access for case studies

--- app/pages/case-studies/[project]/index.vue
+++ app/pages/case-studies/[project]/index.vue
@@ -3,13 +3,11 @@
     <!-- Fixed position items go here -->
     <div ref="pageWrapper">
       <div ref="pageContent">
-        <div v-if="pending">Loading...</div>
-        <div v-else-if="error">Error loading project</div>
-        <div v-else-if="!project">Project not found</div>
+        <div v-if="!project">Project not found</div>
         <div v-else class="w-full flex flex-col items-center">
           <div class="max-w-4xl flex flex-col items-center">
-            <h1 class="text-4xl font-bold mb-(--m-em)">{{ project.title }}</h1>
-            <UITableOfContents :sections='(project.sections as Section[])' class="text-sm rounded-4xl p-(--m-em) bg-mantle-100" />
+            <h1 class="text-4xl font-bold mb-(--m-em)">{{ project.meta.title }}</h1>
+            <UITableOfContents :sections='(project.meta.sections as Section[])' class="text-sm rounded-4xl p-(--m-em) bg-mantle-100" />
           </div>
         </div>
       </div>
@@ -28,13 +26,7 @@ const route = useRoute();
 
 const pageWrapper = ref<HTMLElement | null>(null);
 const pageContent = ref<HTMLElement | null>(null);
+initSmoothScroller(pageWrapper, pageContent);
 
-const project = await queryCollection('caseStudies').first();
-console.log(project);
-
-onMounted(() => {
-  if (pageWrapper.value && pageContent.value) {
-    initSmoothScroller(pageWrapper, pageContent);
-  }
-});
+const project = await queryCollection('caseStudies').where('id', '=', `caseStudies/case-studies/${route.params.project}/${route.params.project}.yml`).first();
 </script>

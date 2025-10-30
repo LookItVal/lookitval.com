4ebeab23ae2ac1185a539cea5c9ed68315eeeb26
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2025-10-30 14:01:45 -0500

    fix: correct typo in Scrollable component and refactor ProseTable to use Scrollable for better layout

--- app/components/UI/Scrollable.vue
+++ app/components/UI/Scrollable.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="overerflow-auto">
+  <div class="overflow-auto">
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup>
 </script>
+
+<style scoped>
+div {
+  /* Customize scrollbar for WebKit browsers */
+  scrollbar-width: thin;
+  scrollbar-color: rgba(100, 100, 100, 0.5) transparent;
+}
+</style>
--- app/components/content/ProseTable.vue
+++ app/components/content/ProseTable.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="w-full overflow-x-auto rounded-4xl text-base md:text-lg">
+  <Scrollable class="w-full rounded-4xl text-base md:text-lg">
     <table>
       <slot />
     </table>
-  </div>
+  </Scrollable>
 </template>
 
 <script lang="ts" setup>
+import Scrollable from '../UI/Scrollable.vue';
+
 </script>
